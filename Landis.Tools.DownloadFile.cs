5a5f3d93e49a53485748511ba1254e181e9ff084
Austen <austen.ruzicka@gmail.com>
2018-04-09 22:49:07 -0700

    Program Complete
    A command line program which downloads from a given url to a given location. If not given two string arguments or if the file is not at its indicated location it throws an exception

---
+++ Landis.Tools.DownloadFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.IO;
+namespace Landis.Tools.DownloadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Submit a url to download from followed by what you want the downloaded file's name to be.");
+                throw new Exception("Program requires two inputs.");
+            }
+            var downloader = new WebClient();
+
+            downloader.DownloadFile(args[0], args[1]);
+
+            if (File.Exists(args[1]))
+            {
+                Console.WriteLine("File download successful");
+            }
+            else
+            {
+                Console.WriteLine("File download failed.");
+                throw new Exception("Download was not successful");
+            }
+
+        }
+    }
+}
